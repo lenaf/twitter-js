5e4292578b54a68c2aa61e9d65f9a46463e74ea6
vinayarv <vinayarv@users.noreply.github.com>
2017-06-13 11:44:30 -0400

    Streamlining

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 const express = require( 'express' );
-const app = express(); 
+const app = express();
 const bodyParser = require('body-parser');
 const chalk = require('chalk');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-const blue = chalk.blue()
-var green = chalk.green()
-const red = chalk.red()
+const blue = chalk.blue
+var green = chalk.green
+const red = chalk.red
+const yellow = chalk.yellow
 
 
 //logging middleware
 
 app.use(function (req, res, next) {
-  console.log(green(req.method), green(req.originalUrl))
+  console.log(res.statusCode);
+  console.log(green(req.method), yellow(req.originalUrl))
   next()
 })
 
+// middleware chain
+app.use('/special/', function(req, res, next){
+    console.log("You reached the special area");
+})
 //a simple route
-app.get('/get-1', function(req, res){
-    res.send('Hello World take 2');
+app.get('/', function(req, res){
+    res.send('Hello World! ' + res.statusCode);
+    // res.statusCode;
+
 });
 
 
@@ -29,6 +37,6 @@ app.post('/post', function(req, res){
 });
 
 //start the server
-app.listen(3000, ()=> 
+app.listen(3000, ()=>
     console.log('server listening on 3000')
 );
